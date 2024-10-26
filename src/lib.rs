d50221755072564e6ad730e52b5628d10d4084c7
Jussi Kukkonen <jkukkonen@google.com>
2024-10-26 16:22:58 +0300

    Refactor
    * Better logging
    * Simplify the detector dispatch: trait objects were not helping

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // TODO
-// * blocking?
-// * handle errors?
-// * less dependencies
+// * is blocking an issue?
+// * less dependencies?
+// * build library only with library deps -- maybe separate into two crates in a workspace?
 
 use std::{collections::HashMap, env, fmt};
 use serde::Deserialize;
@@ -11,14 +11,20 @@ pub type Result<T> = std::result::Result<T, CIIDError>;
 #[derive(Debug, Clone)]
 pub enum CIIDError{
     EnvironmentNotDetected,
-    EnvironmentError,
+    EnvironmentError(String),
     MalformedToken,
 }
 impl fmt::Display for CIIDError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "credential detection failed")
+        match self {
+            CIIDError::EnvironmentError(s) => write!(f, "credential detection failed: {}", s),
+            _ => write!(f, "credential detection failed"),
+        }
     }
 }
+
+type DetectFn = fn(Option<&str>) -> Result<String>;
+
 
 fn validate_token(token: String) -> Result<String> {
     // very, very shallow validation: could this be a JWT token?
@@ -29,24 +35,25 @@ fn validate_token(token: String) -> Result<String> {
 }
 
 pub fn detect_credentials(audience: Option<&str>) -> Result<String> {
-    for detect in [
-        GitHub::detect,
-        GitLab::detect
+    for (name, detect) in [
+        ("GitHub Actions", detect_github as DetectFn),
+        ("GitLab Pipelines", detect_gitlab as DetectFn),
     ] {
         match detect(audience) {
-            Ok(token) => return validate_token(token),
-            Err(CIIDError::EnvironmentNotDetected) => {},
+            Ok(token) => {
+                let token = validate_token(token)?;
+                log::debug!("{}: Token found", name);
+                return Ok(token)
+            },
+            Err(CIIDError::EnvironmentNotDetected) => {
+                log::debug!("{}: Environment not detected", name);
+            },
             Err(e) => return Err(e)
         }
     }
 
     Err(CIIDError::EnvironmentNotDetected)
 }
-
-trait CredentialDetector {
-    fn detect(audience: Option<&str>) -> Result<String>;
-}
-
 
 // Github implementation
 
@@ -55,71 +62,66 @@ struct GitHubTokenResponse {
     value: String,
 }
 
+fn detect_github(audience: Option<&str>) -> Result<String> {
+    if let Err(_) = env::var("GITHUB_ACTIONS") {
+        return Err(CIIDError::EnvironmentNotDetected);
+    };
 
-struct GitHub;
+    let Ok(token_token) = env::var("ACTIONS_ID_TOKEN_REQUEST_TOKEN") else {
+        return Err(CIIDError::EnvironmentError("GitHub Actions: ACTIONS_ID_TOKEN_REQUEST_TOKEN is not set".into()));
+    };
+    let Ok(token_url) = env::var("ACTIONS_ID_TOKEN_REQUEST_URL") else {
+        return Err(CIIDError::EnvironmentError("GitHub Actions: ACTIONS_ID_TOKEN_REQUEST_URL is not set".into()));
+    };
+    let mut params = HashMap::new();
+    if let Some(aud) = audience {
+        params.insert("audience", aud);
+    }
 
-impl CredentialDetector for GitHub {
-    fn detect(audience: Option<&str>) -> Result<String> {
-        log::debug!("Probing for GitHub Actions...");
-
-        if let Err(_) = env::var("GITHUB_ACTIONS") {
-            log::debug!("GitHub Actions environment not detected");
-            return Err(CIIDError::EnvironmentNotDetected);
-        };
-
-        let token_token = env::var("ACTIONS_ID_TOKEN_REQUEST_TOKEN")
-            .expect("ACTIONS_ID_TOKEN_REQUEST_TOKEN env variable not found: is the id-token workflow permission set?");
-        let token_url = env::var("ACTIONS_ID_TOKEN_REQUEST_URL").unwrap();
-        let mut params = HashMap::new();
-        if let Some(aud) = audience {
-            params.insert("audience", aud);
+    log::debug!("GitHub Actions: Requesting token");
+    let client = reqwest::blocking::Client::new();
+    let http_response = match client
+        .get(token_url)
+        .header(
+            reqwest::header::AUTHORIZATION,
+            format!("bearer {}", token_token),
+        )
+        .query(&params)
+        .send() {
+        Ok(response) => response,
+        Err(e) => {
+            return Err(CIIDError::EnvironmentError(format!("GitHub Actions: Token request failed: {}", e)))
         }
 
-        log::debug!("Requesting token");
-        let client = reqwest::blocking::Client::new();
-        let token_response = client
-            .get(token_url)
-            .header(
-                reqwest::header::AUTHORIZATION,
-                format!("bearer {}", token_token),
-            )
-            .query(&params)
-            .send()
-            .unwrap()
-            .json::<GitHubTokenResponse>()
-            .unwrap();
-
-        Ok(token_response.value)
+    };
+    match http_response.json::<GitHubTokenResponse>() {
+        Ok(token_response) => Ok(token_response.value),
+        Err(e) => {
+            Err(CIIDError::EnvironmentError(format!("GitHub Actions: Failed to parse token reponse: {}", e)))
+        }
     }
 }
 
-struct GitLab;
+fn detect_gitlab(audience: Option<&str>) -> Result<String> {
+    // gitlab tokens can be in any environment variable: we require the variable name to be
+    // * "ID_TOKEN" if no audience is argument is used or
+    // * "<AUDIENCE>_ID_TOKEN" where <AUDIENCE> is the audience string.
 
-impl CredentialDetector for GitLab {
-    fn detect(audience: Option<&str>) -> Result<String> {
-        // gitlab tokens can be in any environment variable: we require the variable name to be
-        // * "ID_TOKEN" if no audience is argument is used or
-        // * "<AUDIENCE>_ID_TOKEN" where <AUDIENCE> is the audience string.
+    if let Err(_) = env::var("GITLAB_CI") {
+        return Err(CIIDError::EnvironmentNotDetected);
+    };
 
-        log::debug!("Probing for GitLab Pipelines...");
-
-        if let Err(_) = env::var("GITLAB_CI") {
-            log::debug!("GitLab Pipelines environment not detected");
-            return Err(CIIDError::EnvironmentNotDetected);
-        };
-
-        let var_name = match audience {
-            None => "ID_TOKEN".into(),
-            Some(audience) => {
-                let upper_audience = audience.to_uppercase();
-                let re = Regex::new(r"[^A-Z0-9_]|^[^A-Z_]").unwrap();
-                format!("{}_ID_TOKEN", re.replace_all(&upper_audience, "_"))
-            }
-        };
-        log::debug!("Looking for token in {}", var_name);
-        match env::var(var_name) {
-            Ok(token) => Ok(token),
-            Err(_) => Err(CIIDError::EnvironmentError),
+    let var_name = match audience {
+        None => "ID_TOKEN".into(),
+        Some(audience) => {
+            let upper_audience = audience.to_uppercase();
+            let re = Regex::new(r"[^A-Z0-9_]|^[^A-Z_]").unwrap();
+            format!("{}_ID_TOKEN", re.replace_all(&upper_audience, "_"))
         }
+    };
+    log::debug!("GitLab Pipelines: Looking for token in {}", var_name);
+    match env::var(&var_name) {
+        Ok(token) => Ok(token),
+        Err(_) => Err(CIIDError::EnvironmentError(format!("GitLab Pipelines: {} is not set", var_name))),
     }
 }
